942f023e36bfbc683bae7b368dcb5027a6a7d6d7
JorritNL775 <120494332+JorritNL775@users.noreply.github.com>
2022-12-14 09:31:04 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,10 +3,10 @@ import streamlit
 streamlit.title('First app')
 
 streamlit.header('🥣Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 🐔 🥑🍞 Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 
-🥗 🐔 🥑🍞
+
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
